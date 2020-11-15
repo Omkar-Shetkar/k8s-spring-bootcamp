9128f514a7fb77c0acd45cf61e31274e53ff6b27
Omkar Shetkar <omkar.shetkar@outlook.com>
2020-11-15 14:06:46 +0530

    service discovery with skaffold

--- src/main/java/com/example/demo/K8sDemoAppApplication.java
+++ src/main/java/com/example/demo/K8sDemoAppApplication.java
@@ -2,20 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @RestController
 public class K8sDemoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(K8sDemoAppApplication.class, args);
-	}
+    private RestTemplate rest = new RestTemplateBuilder().build();
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hola World!";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(K8sDemoAppApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        String name = rest.getForObject("http://k8s-workshop-name-service", String.class);
+        return "Hello " + name + " !";
+    }
 }
